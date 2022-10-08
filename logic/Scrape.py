4ab4125a37d6b9917f17a918a0f5a0af0cf32534
dosmaster4life <jefferysuperman@gmail.com>
2022-10-07 19:59:21 -0600

    Scrape.py works and actives

--- logic/Scrape.py
+++ logic/Scrape.py
@@ -1,6 +1,7 @@
 
 # Run shell command "apify run --purge" in the directory HACKATHONSCRAPER4WIN/crawler-google-places-master in python
 import subprocess
+import os
 
 # read file scrape.txt in current directory and set it to a variable
 def scrape():
@@ -8,14 +9,29 @@ def scrape():
         data = file.read().replace('\n', '')
 
     if (data == "false"):
-        subprocess.run(["cd", "../crawler-google-places-master", "apify run --purge"], shell=True)
-        subprocess.run(["pwd"], shell=True)
+        # view the output of the shell command
+
+       # print(subprocess.run(["cd", "../crawler-google-places-master", ";apify run --purge"], shell=True))
         with open('scrape.txt', 'w') as file:
             file.write("true")
+        os.system("cd ../crawler-google-places-master;apify run --purge")
+        # when the shell command is done, set scrape.txt to false
+        # import and run combineJSON.py
+        import combineJSON
+        combineJSON.combine()
+        # output scraping complete
+        print("Scraping Complete")
+        with open('scrape.txt', 'w'):
+            file.write("false")
+
+
+
+
     else:
-        subprocess.run(["cd", "../crawler-google-places-master", "apify run"], shell=True)
-        # get current directory
-        subprocess.run(["pwd"], shell=True)
+        os.system("cd ../crawler-google-places-master;apify run --purge")
+       # print(subprocess.run(["cd", "../crawler-google-places-master", ";apify run"], shell=True))
+
+
     # change subprocces directory
 
 
--- logic/combineJSON.py
+++ logic/combineJSON.py
@@ -2,33 +2,43 @@ import pandas as pd
 import json
 import glob,os
 
-# json folder path 1 directory up from this file
-json_Folder = '../crawler-google-places-master/storage/datasets/default'
-json_Type = os.path.join(json_Folder, '*.json') # get all the json files in the folder
-json_File_List = glob.glob(json_Type) # create a list of the json files
-Data = []
-for file in json_File_List: # loop through the list of json files
-    with open(file) as f:
-        json_data = pd.json_normalize(json.loads(f.read()))
-        json_data['fileName'] = file.rsplit("/", 1)[-1]
-    Data.append(json_data) # append the json data to the list
-allData = pd.concat(Data) # combine the list of json data into one dataframe
-json_Structure = json.loads(allData.to_json(orient="records")) # convert the dataframe to a json structure
+def combine():
+    # json folder path 1 directory up from this file
+    json_Folder = '../crawler-google-places-master/storage/datasets/default'
+    json_Type = os.path.join(json_Folder, '*.json')  # get all the json files in the folder
+    json_File_List = glob.glob(json_Type)  # create a list of the json files
+    Data = []
+    for file in json_File_List:  # loop through the list of json files
+        with open(file) as f:
+            json_data = pd.json_normalize(json.loads(f.read()))
+            json_data['fileName'] = file.rsplit("/", 1)[-1]
+        Data.append(json_data)  # append the json data to the list
+    allData = pd.concat(Data)  # combine the list of json data into one dataframe
+    json_Structure = json.loads(allData.to_json(orient="records"))  # convert the dataframe to a json structure
 
-allData_Flat = pd.io.json.json_normalize(json_Structure) # flatten the json structure
+    allData_Flat = pd.io.json.json_normalize(json_Structure)  # flatten the json structure
 
+    openingRevised = allData_Flat.openingHours.apply(pd.Series)
+    openingRevised.columns = ['Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday',
+                              'Thursday']  # rename the columns
+    openingRevised = openingRevised.applymap(lambda x: x['hours'] if isinstance(x, dict) else x)
+    # convert each phone number to this format: 123.456.7890 and with  extentions only if they exist
+    phone3 = allData_Flat.phone.apply(
+        lambda x: x.replace('(', '').replace(')', '').replace('-', '').replace(' ', '').replace('.',
+                                                                                                ''))  # remove all the characters that are not numbers
+    phone3 = phone3.apply(
+        lambda x: x[:3] + '.' + x[3:6] + '.' + x[6:10] + ' ext.' + x[10:] if len(x) > 10 else x[:3] + '.' + x[
+                                                                                                            3:6] + '.' + x[
+                                                                                                                         6:10])  # add the periods and extension if it exists
+    # rename phone3 to phoneParsed
+    phone3.name = 'Phone Parsed'
+    allData_Flat = pd.concat([allData_Flat, openingRevised, phone3], axis=1)
+    allData_Flat.drop(
+        columns=['orderBy', 'plusCode', 'isAdvertisement', 'rank', 'placeId', 'cid', 'url', 'searchPageUrl',
+                 'reviewsCount', 'reviews', 'reviewsDistribution.oneStar', 'reviewsDistribution.twoStar',
+                 'reviewsDistribution.threeStar', 'reviewsDistribution.fourStar', 'reviewsDistribution.fiveStar',
+                 'price', 'menu', 'temporarilyClosed', 'permanentlyClosed', 'totalScore', 'openingHours', 'subTitle',
+                 'fileName'
+                 ], inplace=True)
 
-openingRevised = allData_Flat.openingHours.apply(pd.Series)
-openingRevised.columns = [ 'Friday', 'Saturday', 'Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday'] # rename the columns
-openingRevised = openingRevised.applymap(lambda x: x['hours'] if isinstance(x, dict) else x)
-# convert each phone number to this format: 123.456.7890 and with  extentions only if they exist
-phone3 = allData_Flat.phone.apply(lambda x: x.replace('(','').replace(')','').replace('-','').replace(' ','').replace('.','')) # remove all the characters that are not numbers
-phone3 = phone3.apply(lambda x: x[:3] + '.' + x[3:6] + '.' + x[6:10] + ' ext.' + x[10:] if len(x) > 10 else x[:3] + '.' + x[3:6] + '.' + x[6:10]) # add the periods and extension if it exists
-#rename phone3 to phoneParsed
-phone3.name = 'Phone Parsed'
-allData_Flat = pd.concat([allData_Flat, openingRevised,phone3], axis=1)
-allData_Flat.drop(columns=['orderBy','plusCode','isAdvertisement','rank','placeId','cid','url','searchPageUrl','reviewsCount','reviews',	'reviewsDistribution.oneStar',	'reviewsDistribution.twoStar','reviewsDistribution.threeStar','reviewsDistribution.fourStar','reviewsDistribution.fiveStar','price','menu','temporarilyClosed','permanentlyClosed','totalScore','openingHours','subTitle','fileName'
-], inplace=True)
-
-
-allData_Flat.to_excel("output.xlsx", index=False)
+    allData_Flat.to_excel("output.xlsx", index=False)
